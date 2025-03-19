db75e4a022f511c95df7d8ebd17bc9dfa0c19e1b
Alex Lurye <allurye@nvidia.com>
2025-03-19 14:00:55 +0100

    Print proper stack trace on crash

--- glean/__main__.py
+++ glean/__main__.py
@@ -17,10 +17,5 @@ import sys
 
 from glean.cmd import main
 
-rc = 1
-try:
-    main()
-    rc = 0
-except Exception as e:
-    print('Error: %s' % e, file=sys.stderr)
-sys.exit(rc)
+main()
+sys.exit(0)
